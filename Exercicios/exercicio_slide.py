47238519a1292606b37823be2acb74ca2130e6bc
RafaelM20 <moreirarafaelgustavo@gmail.com>
2026-01-12 09:42:43 +0000

    Create exercicio_slide.py

---
+++ Exercicios/exercicio_slide.py
@@ -0,0 +1,16 @@
+a1=int(input("Qual o primeiro valor:"))
+a2=int(input("Qual o segundo valor:"))
+op=(input("Qual a operacao desejada (A, M):"))
+
+def adicao(a1,a2):
+    return adicao
+    
+def multiplicacao(a1,a2):
+    return multiplicacao
+
+multiplicacao=a1+a2
+adicao=a1+a2
+    
+print("O valor da adicao é ",adicao)
+
+print("O valor da multiplicacao é ",multiplicacao)

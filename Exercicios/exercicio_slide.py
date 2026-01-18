ab3b1e8ce35e26188ceb87e9e6b41994f3f30f15
RafaelM20 <moreirarafaelgustavo@gmail.com>
2026-01-18 13:33:15 +0000

    Add arithmetic operations exercise

--- Exercicios/exercicio_slide.py
+++ Exercicios/exercicio_slide.py
@@ -1,16 +1,31 @@
-a1=int(input("Qual o primeiro valor:"))
-a2=int(input("Qual o segundo valor:"))
-op=(input("Qual a operacao desejada (A, M):"))
+# Operações aritméticas (adição e multiplicação)
+# Disciplina: Aplicações Informáticas
+# Autor: Rafael M.
 
-def adicao(a1,a2):
-    return adicao
-    
-def multiplicacao(a1,a2):
-    return multiplicacao
 
-multiplicacao=a1+a2
-adicao=a1+a2
-    
-print("O valor da adicao é ",adicao)
+# Leitura dos dois números 
+a1 = int(input("Qual o primeiro valor:"))
+a2 = int(input("Qual o segundo valor:"))
 
-print("O valor da multiplicacao é ",multiplicacao)
+# Menu de opções
+print("Menu de operações:")
+print("A - Adição")
+print("M - Multiplicação")
+
+op = input("Qual a operacao desejada:")
+
+# Definição das funções
+def adicao(a1, a2):
+    return a1 + a2
+
+def multiplicacao(a1, a2):
+    return a1 * a2
+
+if op.upper() == "A":
+    resultado = adicao(a1, a2)
+    print("O valor da adicao é", resultado)
+elif op.upper() == "M":
+    resultado = multiplicacao(a1, a2)
+    print("O valor da multiplicacao é", resultado)
+else:
+    print("Operacao invalida")

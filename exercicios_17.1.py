f606b9a5499e7149c9e21e6e67382782f9c7dd2a
RafaelM20 <moreirarafaelgustavo@gmail.com>
2026-01-18 17:05:41 +0000

    Split exercicios_17.py into separate exercise files

---
+++ exercicios_17.1.py
@@ -0,0 +1,11 @@
+# Cálculo da expressão z = √(x + y)
+# Disciplina: Aplicações Informáticas
+# Exercício 17.1
+# Autor: Rafael M.  
+
+x = float(input("Introduza o valor de x: "))
+y = float(input("Introduza o valor de y: "))
+
+z = (x + y) ** 0.5
+
+print("O valor de z é:", z)
---
+++ exercicios_17.2.py
@@ -0,0 +1,12 @@
+# Cálculo do perímetro de um círculo usando import
+# Disciplina: Aplicações Informáticas
+# Exercício 17.2
+# Autor: Rafael M.
+
+from math import pi
+
+raio = float(input("Indique o valor do raio: "))
+
+perimetro = 2 * pi * raio
+
+print("O perímetro do círculo é:", perimetro)

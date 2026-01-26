9398d6dcab0b32f0db43e02106021dee3ab4279d
RafaelM200 <moreirarafaelgustavo@gmail.com>
2026-01-26 09:39:35 +0000

    Update exercicio_19.1.py

---
+++ exercicio_19.1.py
@@ -0,0 +1,11 @@
+import matplotlib
+
+matplotlib.pyplot.bar()
+
+matplotlib.pyplot.xlabel("Portugal","Brasil","Alemanha","França","Bélgica")
+
+matplotlib.pyplot.ylabel(10,5,7,4,9,1)
+
+matplotlib.pyplot.title("Campeonato do Mundo de Futebol")
+
+matplotlib.pyplot.show()
